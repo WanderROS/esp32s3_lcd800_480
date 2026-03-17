eabaa480a5503a2ebfcc0080be76446140c8a41b
WanderROS <wsp1194409532@gmail.com>
2026-03-17 20:44:14 +0800

    ws2812 点灯示例代码

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
+#include "Adafruit_NeoPixel.h"
 
+#define LED_PIN 4
+#define NUM_LEDS 1
+
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
- Serial.begin(115200);
+  strip.begin();
 }
 
 void loop() {
-  delay(2000);
-  Serial.println("hello world!");
+  strip.setPixelColor(0, 255, 0, 0); // 红色
+  strip.show();
+  delay(1000);
+  
+  strip.setPixelColor(0, 0, 0, 0); // 关闭
+  strip.show();
+  delay(1000);
 }
